23bd4372356f1b579d640266e7c0f44fb410fcc6
naokihayashi <hn14n23@gmail.com>
2026-03-13 11:29:29 +0900

    copy button

---
+++ assets/copy-code.js
@@ -0,0 +1,28 @@
+// This script adds a "Copy" button to each code block on the page, allowing users to easily copy the code to their clipboard.
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  document.querySelectorAll("pre").forEach(function(pre) {
+
+    const wrapper = document.createElement("div");
+    wrapper.className = "code-wrapper";
+
+    pre.parentNode.insertBefore(wrapper, pre);
+    wrapper.appendChild(pre);
+
+    const button = document.createElement("button");
+    button.innerText = "▢";
+    button.className = "copy-button";
+
+    button.onclick = function () {
+      const code = pre.querySelector("code").innerText;
+      navigator.clipboard.writeText(code);
+      button.innerText = "✔";
+      setTimeout(() => button.innerText = "▢", 1200);
+    };
+
+    wrapper.appendChild(button);
+
+  });
+
+});
